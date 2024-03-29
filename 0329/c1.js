9e60c28e83065a5ca003a62302c560fc71f36dc6
noahOU <noah.b.hunter@ou.edu>
2024-03-29 09:23:17 -0500

    j

---
+++ 0329/c1.js
@@ -0,0 +1,28 @@
+"use strict";
+let b1 = document.querySelector("#b1");
+let R = null;
+let chart1 = null;
+async function loadd() {
+    let url = `https://juxinglong.github.io/static/data/states.json`;
+
+    let r = await fetch(url);
+    let rj = await r.json();
+    let c1 = document.querySelector("#c1");
+
+    if (chart1 != null) {
+        chart1.destroy();
+    }
+    c1.innerHTML = ``;
+
+    let opts = {
+        type: "pie",
+        data: {
+            labels: rj.map(x => x.st),
+ datasets: [{ data: rj.map(x => x.p), },], },
+    };
+    chart1 = new Chart(c1, opts);
+    R = rj;
+    console.log(rj);
+    Swal.fire("Load data");
+}
+b1.addEventListener("click", loadd);
